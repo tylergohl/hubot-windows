aba930434fd04582380ca4badde82bd705b8c705
T.J. Gohl <tgohl@defisolutions.com>
2016-06-06 21:13:47 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'hubot-windows'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures hubot-windows'
+long_description 'Installs/Configures hubot-windows'
+version          '0.1.0'
