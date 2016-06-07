ce9230dffbf02d3e8b4f6ad0d0cb0626cbbacf5f
T.J. Gohl <tgohl@defisolutions.com>
2016-06-06 23:16:29 -0500

    install chocolatey, install wmf5

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures hubot-windows'
 long_description 'Installs/Configures hubot-windows'
 version          '0.1.0'
+
+depends 'chocolatey'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: hubot-windows
 # Recipe:: default
 #
-# Copyright (C) 2016 YOUR_NAME
+# Copyright (C) 2016 T.J. Gohl
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'chocolatey'
+
+reboot 'Restart Computer' do
+  action :nothing
+end
+
+chocolatey_package 'powershell' do
+	notifies :reboot_now, 'reboot[Restart Computer]', :immediately
+	version '5.0.10586.20151218'
+end
+
+chocolatey_package 'git.install' do
+	options '--params="\'/NoAutoCrlf\'"'
+end
